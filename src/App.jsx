98eb9ca8c1edfb61755f0daf41ed98579f7b23db
Samroj Khan <samrojk7744@gmail.com>
2026-07-24 10:02:53 +0530

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/home";
+import Search from "./pages/search";
+import Category from "./pages/category";
+import Wallpaper from "./pages/wallpaper";
 
 function App() {
   return (
-    <div className="">GenWalls</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/category/:category" element={<Category />} />
+        <Route path="/wallpaper/:slug" element={<Wallpaper />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
